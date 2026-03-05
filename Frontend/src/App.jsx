3c19b5a540928cec933bc3787aa1ea4f6bc48e1c
Lamichhane-Nirjala <lamichhanenirjala02@gmail.com>
2026-03-05 19:15:20 +0545

    feat: Add main App component with routing and toasts setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,14 @@
 import MainRoute from "./Router/MainRoute";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  return <MainRoute />;
+  return (
+    <>
+      <MainRoute />
+      <ToastContainer position="top-right" autoClose={2000} />
+    </>
+  );
 }
 
 export default App;
